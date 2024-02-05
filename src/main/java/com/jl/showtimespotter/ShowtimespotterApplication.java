bf9c3c4a61ad1552be5d8cf78e1e67f9fb682e2c
Loong <lsloong1012@gmail.com>
2024-02-05 22:00:18 +0800

    ShowtimeSpotter initial setup

---
+++ src/main/java/com/jl/showtimespotter/ShowtimespotterApplication.java
@@ -0,0 +1,13 @@
+package com.jl.showtimespotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowtimespotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowtimespotterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jl/showtimespotter/ShowtimespotterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jl.showtimespotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowtimespotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
